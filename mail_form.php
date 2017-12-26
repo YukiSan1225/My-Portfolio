9342ff1f79d019545b7cac622db4a4abe4583e1b
Damien Burks <dburksgtr@gmail.com>
2017-12-25 18:50:23 -0600

    Finishing Touches & Testing

--- mail_form.php
+++ mail_form.php
@@ -1,18 +1,18 @@
 <?php
 if(isset($_POST['submit'])){
-	$to = "dburksgtr@gmail.com"; //Email address
-	$from = $_POST['email']; // Sender's email address
-	$firstName= $_POST['first_name'];
-	$lastName = $_POST['last_name'];
-	$subject = "Form Submission";
+	$emailAddress = "dburksgtr@gmail.com"; //Email address
+	$senderAddress = $_POST['email']; // Sender's email address
+	$firstName= $_POST['firstName'];
+	$lastName = $_POST['lastName'];
+	$subject = "Questions/Comments?";
 	$subject2 = "Copy of your form submission";
-	$message = $firstName . " " . $lastName . " wrote the following: " / "\n\n" . $_POST['message'];
+	$message = $firstName . " " . $lastName . " wrote the following: " . "\n\n" . $_POST['message'];
 	$message2 = "Here is a copy of your message " . $firstName . "\n\n" . $_POST['message'];
 	
-	$headers = "From:" . $from;
-	$headers2 = "From:" . $to;
-	mail($to,$subject,$message,$headers);
-	mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the senders address
+	$headers = "From:" . $emailAddress;
+	$headers2 = "From:" . $senderAddress;
+	mail($emailAddress,$subject,$message,$headers);
+	mail($senderAddress,$subject2,$message2,$headers2); // sends a copy of the message to the senders address
 	echo "Mail sent. Thank you " . $firstName . ", we will contact you shortly.";
 	
 ?>
